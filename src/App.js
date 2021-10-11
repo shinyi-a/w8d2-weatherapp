5797b1281aa7bda6eb4e7df91d86deefb6e2f956
Shin Yi <contact.shinyi@gmail.com>
2021-10-12 03:55:43 +0800

    spread operator

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 import weatherdata from './weatherData';
 import WeatherIcons from './WeatherIcons';
 
-function LoopData(weatherdata){
-  return (
-    <WeatherIcons
-    key={weatherdata.img}
-    img={weatherdata.img}
-    conditions={weatherdata.conditions}
-    time={weatherdata.time}
-    />
-  );
-}
+// function LoopData(weatherdata){
+//   return (
+//     <WeatherIcons
+//     key={weatherdata.img}
+//     img={weatherdata.img}
+//     conditions={weatherdata.conditions}
+//     time={weatherdata.time}
+//     />
+//   );
+// }
 
 function App() {
   return (
     <div>
       <h1>My WeatherIcons App</h1> 
         <section>
-          {weatherdata.map(LoopData)}
+          {/* {weatherdata.map(LoopData)} */}
+        {weatherdata.map((weather) => {
+          return <WeatherIcons key={weather.img} {...weather}></WeatherIcons>
+        })}
         </section>
     </div>
   );
