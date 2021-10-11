1ed3da4bd2a475d4b48a23b1e155175f58134268
Shin Yi <contact.shinyi@gmail.com>
2021-10-12 02:32:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import weatherdata from './weatherData';
+import WeatherIcons from './WeatherIcons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WeatherIcons
+      img={weatherdata[0].img}
+      conditions={weatherdata[0].conditions}
+      time={weatherdata[0].time}
+      />
+      <WeatherIcons
+      img={weatherdata[1].img}
+      conditions={weatherdata[1].conditions}
+      time={weatherdata[1].time}
+      />
+      <WeatherIcons
+      img={weatherdata[2].img}
+      conditions={weatherdata[2].conditions}
+      time={weatherdata[2].time}
+      />
+      <WeatherIcons
+      img={weatherdata[3].img}
+      conditions={weatherdata[3].conditions}
+      time={weatherdata[3].time}
+      />
+      <WeatherIcons
+      img={weatherdata[4].img}
+      conditions={weatherdata[4].conditions}
+      time={weatherdata[4].time}
+      />
     </div>
   );
 }
---
+++ src/WeatherIcons.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function WeatherIcons(props) {
+    return (
+        <div>
+            <div class="weather">
+                <img
+                src={props.img}
+                alt={props.time}
+                />
+                <p><span>conditions:</span>{props.conditions}</p>
+                <p><span>time:</span>{props.time}</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default WeatherIcons;
